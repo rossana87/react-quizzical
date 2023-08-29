befa4a47cd4521db235f850982356bc7d5bebd4e
Rossana87 <rossana.ventrella@gmail.com>
2023-08-29 11:12:04 +0100

    questions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { nanoid } from 'nanoid'
+import { decode } from 'html-entities'
 import Welcome from './components/Welcome'
 
 
@@ -8,13 +9,27 @@ const App = () => {
 
   const [startQuiz, setStartQuiz] = useState(true)
   const [questions, setQuestions] = useState([])
+  const [selectedAnswers, setSelectedAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [playAgain, setPlayAgain] = useState(false)
+
 
   useEffect(() => {
     const getData = async () => {
       try {
         const response = await axios.get('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple')
         console.log(response.data.results)
+        //const data = response.data.results
         setQuestions(response.data.results)
+        // data.map((item) => ({
+        //   id: nanoid(),
+        //   question: item.question,
+        //   correctAnswer: item.correct_answer,
+        //   incorrectAnswer: item.incorrect_answers,
+        //   selectedAnswer: '',
+        //   checked: false,
+        // }))
+        
       } catch (error) {
         console.log(error)
       }
@@ -22,6 +37,7 @@ const App = () => {
     if (startQuiz === false){
       getData()
     }
+
   },[startQuiz])
 
   const handleStart = () => {
@@ -37,7 +53,7 @@ const App = () => {
             const { id, question, answers } = quiz
             return (
               <div key={id}>
-                <h2>{question}</h2>
+                <h2>{decode(quiz.question)}</h2>
                 <p>{answers}</p>
               </div>
             )
---
+++ src/components/Questions.js
@@ -0,0 +1,8 @@
+
+const Questions = () => {
+  return (
+    <h1>hello</h1>
+  )
+}
+
+export default Questions
