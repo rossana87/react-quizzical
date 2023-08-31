5436d1daf8306e5f23b3c9085e8e0aa049d0da31
Rossana87 <rossana.ventrella@gmail.com>
2023-08-31 06:53:05 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { nanoid } from 'nanoid'
+// import { nanoid } from 'nanoid'
 import { decode } from 'html-entities'
 import Welcome from './components/Welcome'
 
@@ -9,6 +9,7 @@ const App = () => {
 
   const [startQuiz, setStartQuiz] = useState(true)
   const [questions, setQuestions] = useState([])
+  const [correctAnswers, setCorrectAnswers] = useState([])
   const [selectedAnswers, setSelectedAnswers] = useState([])
   const [score, setScore] = useState(0)
   const [playAgain, setPlayAgain] = useState(false)
@@ -18,9 +19,16 @@ const App = () => {
     const getData = async () => {
       try {
         const response = await axios.get('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple')
-        console.log(response.data.results)
+        // console.log(response.data.results)
         //const data = response.data.results
-        setQuestions(response.data.results)
+        setQuestions(() => {
+          const records = response.data.results
+          const results = records.map((record, index) => ({
+            ...record,
+            id: Date.now() + '' + index,
+          }))
+          return results
+        })
       } catch (error) {
         console.log(error)
       }
@@ -31,23 +39,24 @@ const App = () => {
 
   },[startQuiz])
 
+  useEffect(() => {
+    const res = questions.map(question => question.correct_answer)
+    setCorrectAnswers(res)
+  }, [questions.length, setCorrectAnswers])
+
   const handleStart = () => {
     setStartQuiz(false)
   }
 
-  const shuffleArray = (array) => {
-    const shuffledArray = [...array]
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
-    }
-    return shuffledArray
-  }
-
-  const allAnswers = (correctAnswer, incorrectAnswers) => {
-    const mixedAnswers = shuffleArray([correctAnswer, ...incorrectAnswers])
-    return mixedAnswers
-  }
+  // const shuffleArray = (array) => {
+  //   const shuffledArray = [...array] // [1,2]
+  //   for (let i = shuffledArray.length - 1 /* 1*/; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1)); // 1
+  
+  //     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+  //   }
+  //   return shuffledArray
+  // }
   
 
 
@@ -57,19 +66,20 @@ const App = () => {
         {questions.length > 0 ? (
           questions.map((quiz) => {
             // eslint-disable-next-line camelcase
-            const { nanoid, question, correct_answer, incorrect_answers } = quiz
-            const mixedOptions = allAnswers(correct_answer, incorrect_answers)
+            const { id, question, correct_answer, incorrect_answers } = quiz
+            // eslint-disable-next-line camelcase
+            const mixedOptions = [...incorrect_answers, correct_answer]
             return (
-              <div key={nanoid}>
+              <div key={id}>
                 <h2>{decode(question)}</h2>
                 <div className='answers'>
                   {mixedOptions.map((answer, i) => (
                     <label className='btn-answers' key={i}>
                       <input
                         type='radio'
-                        name={`answer_${nanoid}`}
+                        name={`answer_${id}`}
                         value={answer}
-                        checked={selectedAnswers[nanoid] === answer}
+                        checked={selectedAnswers[i] === answer}
                         
                       />
                       {decode(answer)}
